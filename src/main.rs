3c3b4fb43ebefd6abc9a002cf3bc01b2a9ee8c82
John Schmitt <jjshutout19@yahoo.com>
2025-01-06 19:10:48 -0600

    arbitrage calculator working - also allow for entry of money to see how much to place on each leg

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::io::{self, Write};
+
+use arb::RetVals;
 mod arb;
 
 fn main() {
@@ -30,6 +32,39 @@ fn main() {
     // cast second line
     let line_two: i32 = buf.trim().parse::<i32>().unwrap();
 
-    let _ = arb::calc_arb(&line_one, &line_two);
+    // get result from calc_arb and print data accordingly
+    match arb::calc_arb(&line_one, &line_two) {
+        RetVals::ArbAvail(arb_data) => {
+            println!("Arbitrage found! Here is some data about the opportunity:\nPlace {:.2}% on Line 1\nPlace {:.2}% on Line 2\nTotal Profit Percentage: {:.2}%",
+            arb_data.stake_one_precentage * 100.0,
+            arb_data.stake_two_percentage * 100.0,
+            arb_data.profit_percentage,
+            );
+
+            // clear buffer
+            buf.clear();
+
+            // prompt user
+            print!("Enter the amount of money you have available to wager: $");
+            io::stdout().flush().unwrap();
+
+            // get input into the buffer
+            io::stdin().read_line(&mut buf).expect("Error reading wager input");
+
+            let money_avail: f64 = buf.trim().parse::<f64>().unwrap();
+
+            println!("With ${:.2}, you should place {:.2} on Line 1 and {:.2} on Line 2 for a {:.2}% profit!",
+            money_avail,
+            money_avail * arb_data.stake_one_precentage,
+            money_avail * arb_data.stake_two_percentage,
+            arb_data.profit_percentage,
+            );
+
+        },
+        RetVals::NoArb => {
+            println!("No arbitrage was found with the given lines.");
+        }
+    } 
+
 
 }
