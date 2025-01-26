efe3912ebccd472b2d57e29bc23e2e822c962cba
John Schmitt <jjshutout19@yahoo.com>
2025-01-26 16:43:55 -0600

    library is correctly functioning

--- arbet_rust/src/lib.rs
+++ arbet_rust/src/lib.rs
@@ -1,22 +1,23 @@
-use wasn_bindgen::prelude::*;
+use wasm_bindgen::prelude::wasm_bindgen;
 use serde::{Deserialize, Serialize};
+use std::cmp::Ordering;
 
 // used for returns from calc_arb function
-pub enum RetVals{
+enum RetVals{
     ArbAvail(ArbInfo),
     NoArb,
 }
 
-// let 
-pub struct ArbInfo {
-    pub stake_one_precentage: f64,
-    pub stake_two_percentage: f64,
+// lets calc_arb communicate percentages to caller
+struct ArbInfo {
+    pub home_stake_percentage: f64,
+    pub away_stake_percentage: f64,
     pub profit_percentage: f64,
 }
 
 // Structs to hold incoming json
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Spread {
+struct Spread {
     pub book: String,
     pub home_odds: String,
     pub home_spread: String,
@@ -26,7 +27,7 @@ pub struct Spread {
 
 // moneyline struct
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Moneyline {
+struct Moneyline {
     pub book: String,
     pub home_odds: String,
     pub away_odds: String,
@@ -34,83 +35,146 @@ pub struct Moneyline {
 
 // game struct
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Game {
+struct Game {
     pub home_team: String,
     pub away_team: String,
     pub spread: Vec<Spread>,
     pub moneyline: Vec<Moneyline>,
 }
 
-pub type Games = Vec<Game>;
-pub type BooksSearched = Vec<String>;
+type Games = Vec<Game>;
+//type BooksSearched = Vec<String>;
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct Team {
+struct Team {
     pub name: String,
     pub book: String,
+    pub spread: String,
     pub odds: String,
     pub stake: String,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-pub struct ArbReturns {
-    pub arb_avail: String,
+struct ArbReturns {
     pub bet_type: String,
     pub home_team: Team,
     pub away_team: Team,
     pub roi: String,
 }
 
-pub type ReturnData = Vec<ArbReturns>;
+type ReturnData = Vec<ArbReturns>;
 
 #[wasm_bindgen]
-pub fn process_odds(data: &str, books_searched: &str) -> String {
+pub fn process_odds(data: &str) -> String {
 
     // parse incoming json data
     let betting_data: Games = serde_json::from_str(data).unwrap();
 
     // get list of books searched, sent from json
-    let books: BooksSearched = serde_json::from_str(books_searched).unwrap();
+    //let books: BooksSearched = serde_json::from_str(books_searched).unwrap();
 
-    let arb_ret: ReturnData = vec![];
+    let mut arb_ret: ReturnData = vec![];
 
     // betting data holds all of the information we need about the games
     // now need to go through for each game and see if there is arb avail
-    for (i, game_log) in betting_data.iter().enumerate(){
+    for (_, game_log) in betting_data.iter().enumerate(){
         // for each game, find best odds between the 3 sites
-        let curr_game: Game = game_log.get(i);
-
-        // deal with finding best spread for each team
-        let curr_game_spread: Vec<Spread> = curr_game.spread;
+        let curr_game: &Game = game_log;
 
         // find best home odds
-        let max_home_odds = curr_game_spread
+        let max_home_odds = curr_game.spread
             .iter()
-            .filter_map(|s| s.home_odds.parse::<f64>().ok())
-            .max_by(|a, b| a.partial_cmp(b).unwrap_or(Ordering::Equal));
+            .enumerate()
+            .filter_map(|(index, s)| {
+                s.home_odds.parse::<f64>()
+                    .ok()
+                    .map(|odds| (odds, index))
+            })
+            .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap_or(Ordering::Equal));
 
         match max_home_odds {
-            Some(max) => println!("Max home odds: {}", max),
+            Some((odds, _)) => println!("Max home odds: {}", odds),
             None => println!("No valid home odds found."),
         }
 
+        let max_away_odds = curr_game.spread
+        .iter()
+        .enumerate()
+        .filter_map(|(index, s)| {
+            s.away_odds.parse::<f64>()
+                .ok()
+                .map(|odds| (odds, index)) 
+        })
+        .max_by(|a, b| a.0.partial_cmp(&b.0).unwrap_or(Ordering::Equal));
 
-        let curr_game_moneyline: Vec<Moneyline> = curr_game.moneyline;
+        match max_away_odds {
+            Some((odds, _)) => println!("Max away odds: {}", odds),
+            None => println!("No valid away odds found."),
+        }
 
+        // -- correctly getting the both max odds --
 
-        // send the best odds to the calc_arb function, which can be moved into this file
+        // send the best odds to the calc_arb function, 
+        match (max_home_odds, max_away_odds){
+            (Some((home_odds, home_idx)), Some((away_odds, away_idx))) => {
 
-        // if arb is avail, create ArbVals instance 
+                match calc_arb(&home_odds, &away_odds){
+                    RetVals::ArbAvail(data) => {
+                        // create home team
+                        let home_team : Team = Team {
+                            name: curr_game.home_team.clone(),
+                            book: curr_game.spread
+                                .get(home_idx)
+                                .map(|s| s.book.clone())
+                                .unwrap_or_else(||"Unknown".to_string()),
+                            spread: curr_game.spread
+                                .get(home_idx)
+                                .map(|s| s.home_spread.clone())
+                                .unwrap_or_else(|| "Unknown".to_string()),
+                            odds: home_odds.to_string(),
+                            stake: (data.home_stake_percentage * 100.0).to_string()
+                        };
 
-        // push ArbReturns to arb_ret
+                        let away_team : Team = Team {
+                            name: curr_game.away_team.clone(),
+                            book: curr_game.spread
+                                .get(away_idx)
+                                .map(|s| s.book.clone())
+                                .unwrap_or_else(||"Unknown".to_string()),
+                            spread: curr_game.spread
+                                .get(away_idx)
+                                .map(|s| s.away_spread.clone())
+                                .unwrap_or_else(|| "Unknown".to_string()),
+                            odds: away_odds.to_string(),
+                            stake: (data.away_stake_percentage * 100.0).to_string()
+                        };
+
+                        let arb_opp : ArbReturns = ArbReturns {
+                            bet_type: "Spread".to_string(),
+                            home_team: home_team,
+                            away_team : away_team,
+                            roi: data.profit_percentage.to_string()
+                        };
+
+                        arb_ret.push(arb_opp);
+
+                    }
+                    _ => {}
+                }
+            }
+            _ => {}
+  
+        }
     }
+
+    serde_json::to_string(&arb_ret).unwrap()
 
 }
 
 // func for getting implient probability
-pub fn calc_implied_prob(line: &i32) -> f64 {
-    let odds:i32 = *line;
-    if odds > 0 {
+fn calc_implied_prob(line: &f64) -> f64 {
+    let odds:f64 = *line;
+    if odds > 0.0 {
         100.0 / (odds as f64 + 100.0)
     }
     else{
@@ -119,20 +183,20 @@ pub fn calc_implied_prob(line: &i32) -> f64 {
 }
 
 // func to see if arb is available or not
-pub fn calc_arb(line_one: &i32, line_two: &i32) -> RetVals {
-    let prob_one: f64 = calc_implied_prob(line_one);
-    let prob_two: f64 = calc_implied_prob(line_two);
-    let total_prob: f64 = prob_one + prob_two;
+fn calc_arb(home_line: &f64, away_line: &f64) -> RetVals {
+    let prob_home: f64 = calc_implied_prob(home_line);
+    let prob_away: f64 = calc_implied_prob(away_line);
+    let total_prob: f64 = prob_home + prob_away;
 
     if total_prob < 1.0 {
-        let stake_one: f64 = prob_one / total_prob;
-        let stake_two: f64 = 1.0 - stake_one;
+        let stake_home: f64 = prob_home / total_prob;
+        let stake_away: f64 = 1.0 - stake_home;
 
         let profit_percentage: f64 = (1.0 / total_prob - 1.0) * 100.0;
 
         let arb_data: ArbInfo = ArbInfo {
-            stake_one_precentage: stake_one,
-            stake_two_percentage: stake_two,
+            home_stake_percentage: stake_home,
+            away_stake_percentage: stake_away,
             profit_percentage: profit_percentage,
         };
 
--- src/main.rs
+++ src/main.rs
@@ -1,70 +1,255 @@
-use std::io::{self, Write};
-
-use arb::RetVals;
-mod arb;
+//use std::io::{self, Write};
+use arbet_rust::process_odds;
+use serde_json::{self, Value};
 
 fn main() {
     println!("Welcome to arbet, your personal arbitrage betting finder!");
 
     // buffer for taking input
-    let mut buf = String::new();
+    // let mut buf = String::new();
 
-    // print prompt for first line
-    print!("Line 1: ");
-    io::stdout().flush().unwrap();
+    // // print prompt for first line
+    // print!("Line 1: ");
+    // io::stdout().flush().unwrap();
 
-    // get input for first line
-    io::stdin().read_line(&mut buf).expect("Error reading input");
+    // // get input for first line
+    // io::stdin().read_line(&mut buf).expect("Error reading input");
 
-    // cast to int
-    let line_one: i32 = buf.trim().parse::<i32>().unwrap();
+    // // cast to int
+    // let line_one: i32 = buf.trim().parse::<i32>().unwrap();
 
-    // clear the buffer
-    buf.clear();
+    // // clear the buffer
+    // buf.clear();
 
-    // prompt for second line
-    print!("Line 2: ");
-    io::stdout().flush().unwrap();
+    // // prompt for second line
+    // print!("Line 2: ");
+    // io::stdout().flush().unwrap();
 
-    // read second line
-    io::stdin().read_line(&mut buf).expect("Error reading input");
+    // // read second line
+    // io::stdin().read_line(&mut buf).expect("Error reading input");
 
-    // cast second line
-    let line_two: i32 = buf.trim().parse::<i32>().unwrap();
+    // // cast second line
+    // let line_two: i32 = buf.trim().parse::<i32>().unwrap();
 
-    // get result from calc_arb and print data accordingly
-    match arb::calc_arb(&line_one, &line_two) {
-        RetVals::ArbAvail(arb_data) => {
-            println!("Arbitrage found! Here is some data about the opportunity:\nPlace {:.2}% on Line 1\nPlace {:.2}% on Line 2\nTotal Profit Percentage: {:.2}%",
-            arb_data.stake_one_precentage * 100.0,
-            arb_data.stake_two_percentage * 100.0,
-            arb_data.profit_percentage,
-            );
+    // ------------------ Testing module below --------------------------
+    let test_json_data = r#"
+[
+    {
+        "home_team": "LA Rams",
+        "away_team": "Minnesota Vikings",
+        "spread": [
+            {
+                "book": "DraftKings",
+                "home_odds": "110",
+                "home_spread": "2.5",
+                "away_odds": "-118",
+                "away_spread": "-2.5"
+            },
+            {
+                "book": "Fanduel",
+                "home_odds": "-101",
+                "home_spread": "2.5",
+                "away_odds": "-118",
+                "away_spread": "-2.5"
+            },
+            {
+                "book": "MGM",
+                "home_odds": "-120",
+                "home_spread": "2.5",
+                "away_odds": "-101",
+                "away_spread": "-2.5"
+            }
+        ],
+        "moneyline": [
+            {
+                "book": "Draftkings",
+                "home_odds": "120",
+                "away_odds": "-142"
+            },
+            {
+                "book": "Fanduel",
+                "home_odds": "122",
+                "away_odds": "-144"
+            },
+            {
+                "book": "MGM",
+                "home_odds": "127",
+                "away_odds": "-147"
+            }
+        ]
+    },
+    {
+        "home_team": "Kansas City Chiefs",
+        "away_team": "Buffalo Bills",
+        "spread": [
+            {
+                "book": "DraftKings",
+                "home_odds": "200",
+                "home_spread": "1.5",
+                "away_odds": "-115",
+                "away_spread": "-1.5"
+            },
+            {
+                "book": "Fanduel",
+                "home_odds": "-110",
+                "home_spread": "1.5",
+                "away_odds": "120",
+                "away_spread": "-1.5"
+            }
+        ],
+        "moneyline": [
+            {
+                "book": "Draftkings",
+                "home_odds": "100",
+                "away_odds": "-120"
+            },
+            {
+                "book": "Fanduel",
+                "home_odds": "-102",
+                "away_odds": "-118"
+            }
+        ]
+    },
+    {
+        "home_team": "New York Giants",
+        "away_team": "Philadelphia Eagles",
+        "spread": [
+            {
+                "book": "DraftKings",
+                "home_odds": "-115",
+                "home_spread": "3.5",
+                "away_odds": "-105",
+                "away_spread": "-3.5"
+            },
+            {
+                "book": "Fanduel",
+                "home_odds": "-108",
+                "home_spread": "3.5",
+                "away_odds": "-108",
+                "away_spread": "-3.5"
+            },
+            {
+                "book": "MGM",
+                "home_odds": "-110",
+                "home_spread": "3.5",
+                "away_odds": "-110",
+                "away_spread": "-3.5"
+            }
+        ],
+        "moneyline": [
+            {
+                "book": "Draftkings",
+                "home_odds": "135",
+                "away_odds": "-155"
+            },
+            {
+                "book": "Fanduel",
+                "home_odds": "140",
+                "away_odds": "-160"
+            }
+        ]
+    },
+    {
+        "home_team": "Dallas Cowboys",
+        "away_team": "San Francisco 49ers",
+        "spread": [
+            {
+                "book": "DraftKings",
+                "home_odds": "100",
+                "home_spread": "0.0",
+                "away_odds": "-110",
+                "away_spread": "0.0"
+            },
+            {
+                "book": "Fanduel",
+                "home_odds": "105",
+                "home_spread": "0.0",
+                "away_odds": "-120",
+                "away_spread": "0.0"
+            }
+        ],
+        "moneyline": [
+            {
+                "book": "Draftkings",
+                "home_odds": "100",
+                "away_odds": "-120"
+            },
+            {
+                "book": "Fanduel",
+                "home_odds": "105",
+                "away_odds": "-125"
+            }
+        ]
+    },
+    {
+        "home_team": "Green Bay Packers",
+        "away_team": "Chicago Bears",
+        "spread": [
+            {
+                "book": "DraftKings",
+                "home_odds": "-110",
+                "home_spread": "4.0",
+                "away_odds": "+105",
+                "away_spread": "-4.0"
+            },
+            {
+                "book": "Fanduel",
+                "home_odds": "-110",
+                "home_spread": "4.0",
+                "away_odds": "-110",
+                "away_spread": "-4.0"
+            },
+            {
+                "book": "MGM",
+                "home_odds": "-115",
+                "home_spread": "4.0",
+                "away_odds": "200",
+                "away_spread": "-4.0"
+            }
+        ],
+        "moneyline": [
+            {
+                "book": "Draftkings",
+                "home_odds": "200",
+                "away_odds": "-250"
+            },
+            {
+                "book": "Fanduel",
+                "home_odds": "210",
+                "away_odds": "-260"
+            },
+            {
+                "book": "MGM",
+                "home_odds": "220",
+                "away_odds": "-270"
+            }
+        ]
+    }
+]
 
-            // clear buffer
-            buf.clear();
+"#;
 
-            // prompt user
-            print!("Enter the amount of money you have available to wager: $");
-            io::stdout().flush().unwrap();
+// TODO(jj): ---------- ^^ fix above json for arbitrage, gpt stinks --------------
 
-            // get input into the buffer
-            io::stdin().read_line(&mut buf).expect("Error reading wager input");
+let arbitrage_opps: String = process_odds(&test_json_data);
 
-            let money_avail: f64 = buf.trim().parse::<f64>().unwrap();
+if arbitrage_opps.trim().is_empty() || arbitrage_opps.trim() == "[]" {
+    println!("There are no available arbitrage opportunities at this time.")
+}
+else{
 
-            println!("With ${:.2}, you should place ${:.2} on Line 1 and ${:.2} on Line 2 for a {:.2}% profit!",
-            money_avail,
-            money_avail * arb_data.stake_one_precentage,
-            money_avail * arb_data.stake_two_percentage,
-            arb_data.profit_percentage,
-            );
-
-        },
-        RetVals::NoArb => {
-            println!("No arbitrage was found with the given lines.");
+    match serde_json::from_str::<Value>(&arbitrage_opps){
+        Ok(parsed_json) => {
+            println!("{}", serde_json::to_string_pretty(&parsed_json).unwrap());
         }
-    } 
+        Err(_) => {
+            println!("There was an error parsing the JSON return.");
+        }
+    }
+    
+}
+
+
 
 
 }
