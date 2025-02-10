6a05c4018ec970a8a8df460a9e01ec73deaef494
John Schmitt <jjshutout19@yahoo.com>
2025-02-10 16:43:46 -0600

    small change for simplicity reasons, also set env perm env var for arbet

--- arbet_js/src/apiClient.js
+++ arbet_js/src/apiClient.js
@@ -3,19 +3,15 @@ const apiKey = process.env.ODDS_API_KEY;
 
 // Date math
 const currDate = new Date().toISOString().split('T')[0];
-const commenceFrom = currDate + "T12:00:00Z";
+const commenceFrom = currDate + "T12%3A00%3A00Z";
 const tmrw = new Date()
 tmrw.setDate(tmrw.getDate() + 1);
-const commenceTo = tmrw.toISOString().split('T')[0] + "T07:00:00Z";
-
-// regex to replace : with "%3A" in html request
-const commenceFromFormat = commenceFrom.replace(/:\s*/g, "%3A");
-const commenceToFormat = commenceTo.replace(/:\s*/g, "%3A");
+const commenceTo = tmrw.toISOString().split('T')[0] + "T07%3A00%3A00Z";
 
 async function fetchData(sport) {
       try {
         // replace with the correct api link
-        const response = await axios.get(`https://api.the-odds-api.com/v4/sports/${sport}/odds/?regions=us&markets=h2h&apiKey=${apiKey}&oddsFormat=american&bookmakers=draftkings%2Cfanduel%2Cbetmgm%2Cespnbet%2Chardrockbet&commenceTimeFrom=${commenceFromFormat}&commenceTimeTo=${commenceToFormat}`);
+        const response = await axios.get(`https://api.the-odds-api.com/v4/sports/${sport}/odds/?regions=us&markets=h2h&apiKey=${apiKey}&oddsFormat=american&bookmakers=draftkings%2Cfanduel%2Cbetmgm%2Cespnbet%2Chardrockbet&commenceTimeFrom=${commenceFrom}&commenceTimeTo=${commenceTo}`);
         return response.data;
       } catch (error) {
         console.error('Error fetching API data!!');
